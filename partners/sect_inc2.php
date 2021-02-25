597bf2d6fc473be0709be9087a57492cc2f19ee8
SoftElit <mobcomm@yandex.ru>
2021-02-25 15:03:21 +0300

    Areas to include

---
+++ partners/sect_inc2.php
@@ -0,0 +1,4 @@
+<div class="sb_event">
+						<div class="sb_event_header"><h4>Внимание!</h4></div>
+						<p><a href="/partners/conditions-of-partnership.php">Заключение партнерского договора позволит вам вывести бизнес на новый уровень.</p>
+					</div>
