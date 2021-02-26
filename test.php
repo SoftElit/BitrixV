ce2f5f34533ed20651d916252ff3f8ea98cef2c2
SoftElit <mobcomm@yandex.ru>
2021-02-26 21:02:32 +0300

    Authorization and Contacts

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
+	"FORGOT_PASSWORD_URL" => "/user/",	// Страница забытого пароля
+		"PROFILE_URL" => "/user/profile.php",	// Страница профиля
+		"REGISTER_URL" => "/user/registration.php",	// Страница регистрации
+		"SHOW_ERRORS" => "Y",	// Показывать ошибки
+	),
+	false
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
