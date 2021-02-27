b4bc5580cd079f93511625cf1aa330e62007348f
SoftElit <mobcomm@yandex.ru>
2021-02-27 15:34:27 +0300

    debugging functions

--- products/index.php
+++ products/index.php
@@ -2,81 +2,81 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 // $APPLICATION->SetTitle("Продукция"); /* вместо этой строки установим в следующей строке параметр "NOT_SHOW_NAV_CHAIN" для скрытия навигационной цепочки в каталоге products (Каталог , "Продукция") */
  $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN","Y"); /* установим параметр "NOT_SHOW_NAV_CHAIN" для скрытия навигационной цепочки в каталоге products (Каталог , "Продукция") */
-?><?$APPLICATION->IncludeComponent("bitrix:catalog", ".default", array(
-	"IBLOCK_TYPE" => "products",
-	"IBLOCK_ID" => "2",
-	"BASKET_URL" => "",
-	"ACTION_VARIABLE" => "action",
-	"PRODUCT_ID_VARIABLE" => "id",
-	"SECTION_ID_VARIABLE" => "SECTION_ID",
-	"SEF_MODE" => "Y",
-	"SEF_FOLDER" => "/products/",
-	"AJAX_MODE" => "N",
-	"AJAX_OPTION_SHADOW" => "Y",
-	"AJAX_OPTION_JUMP" => "N",
-	"AJAX_OPTION_STYLE" => "Y",
-	"AJAX_OPTION_HISTORY" => "N",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"CACHE_FILTER" => "N",
-	"CACHE_GROUPS" => "Y",
-	"DISPLAY_PANEL" => "N",
-	"SECTION_SHOW_PARENT_NAME" => "N",
-	"SET_TITLE" => "Y",
-	"SET_STATUS_404" => "Y",
-	"USE_FILTER" => "N",
-	"USE_COMPARE" => "N",
-	"PRICE_CODE" => array(
-		0 => "PRICE",
-	),
-	"USE_PRICE_COUNT" => "N",
-	"SHOW_PRICE_COUNT" => "1",
-	"PRICE_VAT_INCLUDE" => "N",
-	"PRICE_VAT_SHOW_VALUE" => "N",
-	"SHOW_TOP_ELEMENTS" => "N",
-	"PAGE_ELEMENT_COUNT" => "10",
-	"LINE_ELEMENT_COUNT" => "1",
-	"ELEMENT_SORT_FIELD" => "sort",
-	"ELEMENT_SORT_ORDER" => "asc",
-	"LIST_PROPERTY_CODE" => array(
-		0 => "PRICECURRENCY",
-		1 => "",
-	),
-	"INCLUDE_SUBSECTIONS" => "Y",
-	"LIST_META_KEYWORDS" => "-",
-	"LIST_META_DESCRIPTION" => "-",
-	"LIST_BROWSER_TITLE" => "-",
-	"DETAIL_PROPERTY_CODE" => array(
-		0 => "SIZE",
-		1 => "S_SIZE",
-		2 => "ARTNUMBER",
-		3 => "MATERIAL",
-		4 => "MANUFACTURER",
-		5 => "",
-	),
-	"DETAIL_META_KEYWORDS" => "-",
-	"DETAIL_META_DESCRIPTION" => "-",
-	"DETAIL_BROWSER_TITLE" => "-",
-	"DETAIL_SHOW_PICTURE" => "Y",
-	"LINK_IBLOCK_TYPE" => "",
-	"LINK_IBLOCK_ID" => "",
-	"LINK_PROPERTY_SID" => "",
-	"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",
-	"DISPLAY_TOP_PAGER" => "N",
-	"DISPLAY_BOTTOM_PAGER" => "Y",
-	"PAGER_TITLE" => "Продукция",
-	"PAGER_SHOW_ALWAYS" => "N",
-	"PAGER_TEMPLATE" => "arrows",
-	"PAGER_DESC_NUMBERING" => "N",
-	"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000000",
-	"PAGER_SHOW_ALL" => "N",
-	"AJAX_OPTION_ADDITIONAL" => "",
-	"SEF_URL_TEMPLATES" => array(
-		"sections" => "",
-		"section" => "#SECTION_ID#/",
-		"element" => "#SECTION_ID#/#ELEMENT_ID#/",
-		"compare" => "",
-	)
+?><?$APPLICATION->IncludeComponent("bitrix:catalog", "template1", Array(
+	"IBLOCK_TYPE" => "products",	// Тип инфоблока
+		"IBLOCK_ID" => "2",	// Инфоблок
+		"BASKET_URL" => "",	// URL, ведущий на страницу с корзиной покупателя
+		"ACTION_VARIABLE" => "action",	// Название переменной, в которой передается действие
+		"PRODUCT_ID_VARIABLE" => "id",	// Название переменной, в которой передается код товара для покупки
+		"SECTION_ID_VARIABLE" => "SECTION_ID",	// Название переменной, в которой передается код группы
+		"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
+		"SEF_FOLDER" => "/products/",	// Каталог ЧПУ (относительно корня сайта)
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_SHADOW" => "Y",
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"DISPLAY_PANEL" => "N",
+		"SECTION_SHOW_PARENT_NAME" => "N",
+		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"SET_STATUS_404" => "Y",	// Устанавливать статус 404
+		"USE_FILTER" => "N",	// Показывать фильтр
+		"USE_COMPARE" => "N",	// Разрешить сравнение товаров
+		"PRICE_CODE" => array(	// Тип цены
+			0 => "PRICE",
+		),
+		"USE_PRICE_COUNT" => "N",	// Использовать вывод цен с диапазонами
+		"SHOW_PRICE_COUNT" => "1",	// Выводить цены для количества
+		"PRICE_VAT_INCLUDE" => "N",	// Включать НДС в цену
+		"PRICE_VAT_SHOW_VALUE" => "N",	// Отображать значение НДС
+		"SHOW_TOP_ELEMENTS" => "N",	// Выводить топ элементов
+		"PAGE_ELEMENT_COUNT" => "10",	// Количество элементов на странице
+		"LINE_ELEMENT_COUNT" => "1",	// Количество элементов, выводимых в одной строке таблицы
+		"ELEMENT_SORT_FIELD" => "sort",	// По какому полю сортируем товары в разделе
+		"ELEMENT_SORT_ORDER" => "asc",	// Порядок сортировки товаров в разделе
+		"LIST_PROPERTY_CODE" => array(
+			0 => "PRICECURRENCY",
+			1 => "",
+		),
+		"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
+		"LIST_META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства раздела
+		"LIST_META_DESCRIPTION" => "-",	// Установить описание страницы из свойства раздела
+		"LIST_BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства раздела
+		"DETAIL_PROPERTY_CODE" => array(
+			0 => "SIZE",
+			1 => "S_SIZE",
+			2 => "ARTNUMBER",
+			3 => "MATERIAL",
+			4 => "MANUFACTURER",
+			5 => "",
+		),
+		"DETAIL_META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+		"DETAIL_META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+		"DETAIL_BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+		"DETAIL_SHOW_PICTURE" => "Y",
+		"LINK_IBLOCK_TYPE" => "",	// Тип инфоблока, элементы которого связаны с текущим элементом
+		"LINK_IBLOCK_ID" => "",	// ID инфоблока, элементы которого связаны с текущим элементом
+		"LINK_PROPERTY_SID" => "",	// Свойство, в котором хранится связь
+		"LINK_ELEMENTS_URL" => "link.php?PARENT_ELEMENT_ID=#ELEMENT_ID#",	// URL на страницу, где будет показан список связанных элементов
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"DISPLAY_BOTTOM_PAGER" => "Y",	// Выводить под списком
+		"PAGER_TITLE" => "Продукция",	// Название категорий
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => "arrows",	// Шаблон постраничной навигации
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"SEF_URL_TEMPLATES" => array(
+			"sections" => "",
+			"section" => "#SECTION_ID#/",
+			"element" => "#SECTION_ID#/#ELEMENT_ID#/",
+			"compare" => "",
+		)
 	),
 	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
